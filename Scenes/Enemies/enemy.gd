d3dafef37ec4ab6f8400bab068b3d25a0e81559c
Sander Lotuft <69865773+Snadderbyte@users.noreply.github.com>
2025-01-25 21:12:58 +0100

    Fix tower rotation, points towards target, flips accordingly
    Commit by Kjetil, bullet targeting still different than tower's

--- Scenes/Enemies/enemy.gd
+++ Scenes/Enemies/enemy.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
 
-@export var speed = 200
+@export var speed = 300
 var health = 10
-
 
 func _physics_process(delta: float) -> void:
 	get_parent().set_progress(get_parent().get_progress() + speed * delta)
+	var old_rotation = get_parent().rotation
+	get_parent().rotation = 0
+	
+	scale.x = abs(scale.x)
+	scale.x *= -1 if old_rotation >= 0 else 1
 	
 	var dead = get_parent().get_progress_ratio() == 1 or health <= 0
 	if dead:
--- Scenes/Towers/basic_bubble_tower.gd
+++ Scenes/Towers/basic_bubble_tower.gd
@@ -10,6 +10,18 @@ var frames = 0
 func _process(delta: float) -> void:
 	if is_instance_valid(currentTarget):
 		self.look_at(currentTarget.global_position)
+		rotation += PI # Rotate around 180 deg
+		while rotation < 0: rotation += 2 * PI
+		while rotation >= 2 * PI: rotation -= 2 * PI # modulo 2 * PI doesn't work...
+		
+		var sprite = get_parent().get_child(0)
+		sprite.scale.x = abs(sprite.scale.x)
+		var flip = rotation >= PI / 2 and rotation < PI * 3 / 2
+		if flip:
+			sprite.scale.x *= -1
+			sprite.rotation = rotation - PI
+		else:
+			sprite.rotation = rotation
 
 func _physics_process(delta: float) -> void:
 	frames += 1
