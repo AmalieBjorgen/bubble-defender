d1f98f4a743d4d13b87a22a831a0eb0dbbd34d76
Sander Lotuft <69865773+Snadderbyte@users.noreply.github.com>
2025-01-26 15:07:42 +0100

    Add health scaling in waves + boss fight
    Commit by Kjetil

--- Scripts/timer.gd
+++ Scripts/timer.gd
@@ -10,15 +10,33 @@ func _ready():
 
 func wave_start():
 	var wave = get_node("../GameStats").wave_current
+	var last_wave = get_node("../GameStats").wave_max
+	
+	var boss_wave = wave == last_wave
 	
 	var enemy_count = 5 + floor(wave * wave / 1.3)
 	for i in enemy_count:
 		var type = lcg() % 2
+		var kid = null
 		if type == 0:
-			add_child(path_barrel.instantiate())
+			kid = path_barrel.instantiate()
 		else:
-			add_child(path_ketchup.instantiate())
+			kid = path_ketchup.instantiate()
+		kid.get_child(0).get_child(0).health *= 1 + 0.2 * wave
+		add_child(kid)
 		await get_tree().create_timer(0.5).timeout
+	
+	if boss_wave:
+		var kid = path_barrel.instantiate()
+		var kidc = kid.get_child(0).get_child(0)
+		kidc.health *= 10
+		kidc.scale.x *= 3
+		kidc.scale.y *= 3
+		kidc.damage *= 3
+		kidc.speed /= 2
+		add_child(kid)
+		enemy_count += 1
+	
 	get_node("../GameStats").on_wave_spawning_complete(enemy_count)
 
 # Linear Congruential Generator, seed = (a * seed + c) % mod. glibc uses a, c and mod shown below
